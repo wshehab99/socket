a9717b5bd005dde4786649685108e6905db4dac3
unknown <waleedbinshihab@gmail.com>
2021-11-13 01:05:25 +0200

    second commit

---
+++ client/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send = new System.Windows.Forms.Button();
+            this.messege = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(240, 283);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(77, 41);
+            this.send.TabIndex = 0;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // messege
+            // 
+            this.messege.Location = new System.Drawing.Point(3, 284);
+            this.messege.Multiline = true;
+            this.messege.Name = "messege";
+            this.messege.Size = new System.Drawing.Size(231, 40);
+            this.messege.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 275);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 330);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.messege);
+            this.Controls.Add(this.send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox messege;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ client/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        Socket clientSocket;
+        IPEndPoint serverIpAddress;
+        public Form1()
+        {
+            InitializeComponent();
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverIpAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"),6767);
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            if (messege.Text.Trim()==null)
+            {
+                MessageBox.Show("empty");
+                messege.Focus();
+                return;
+            }
+            byte[] buffer = Encoding.ASCII.GetBytes(messege.Text.Trim());
+            clientSocket.SendTo(buffer, serverIpAddress);
+            messege.Text = "";
+        }
+    }
+}
