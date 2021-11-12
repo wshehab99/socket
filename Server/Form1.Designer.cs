4c21c3c58380ddf3ee9edd9bf5762cdf5846111a
unknown <waleedbinshihab@gmail.com>
2021-11-13 01:01:58 +0200

    first commit

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.showMesseges = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(346, 4);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(129, 48);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(12, 4);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(129, 48);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 368);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // showMesseges
+            // 
+            this.showMesseges.Location = new System.Drawing.Point(12, 58);
+            this.showMesseges.Multiline = true;
+            this.showMesseges.Name = "showMesseges";
+            this.showMesseges.Size = new System.Drawing.Size(464, 304);
+            this.showMesseges.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 397);
+            this.Controls.Add(this.showMesseges);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox showMesseges;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        //initilize socket
+        Socket serverSocket;
+        IPEndPoint serverAddress;
+        Thread thread;
+        void startServer()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            
+            serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6767);
+            //bind : make ip address contained ip and port number
+            serverSocket.Bind(serverAddress);
+            //ThreadStart(Listning) run lisning method in that thread
+            thread = new Thread(new ThreadStart(Listening));
+            //make server start to listen
+            thread.Start();
+
+        }
+        bool isRunning = false;
+        void Listening()
+        {
+            while (isRunning)
+            {
+                byte[] buffer = new byte[4096];
+                //ip address any to recieve from any ip address and port 0 to recieve from any port number
+                EndPoint clientAddress = new IPEndPoint(IPAddress.Any, 0);
+                //receive from client address and put data on buffer
+                int byteCount = serverSocket.ReceiveFrom(buffer, ref clientAddress);
+                String msg = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                
+              
+                showMesseges.Invoke(new Action(delegate
+                {
+                    showMesseges.AppendText(clientAddress.ToString() + " : " + msg + "\n");
+                }));
+            }
+
+        }
+        void stopServer()
+        {
+
+            isRunning = false;
+            //stop thread
+            thread.Abort();
+            //close socket
+            serverSocket.Close();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Dissconnected";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+
+            start.Enabled = true;
+            stop.Enabled = false;
+            textBox1.Text = "dissconnected";
+            textBox1.ForeColor = Color.Red;
+            stopServer();
+            
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Enabled = false;
+            stop.Enabled = true;
+            textBox1.Text = "Connected";
+            textBox1.ForeColor = Color.Green;
+            startServer();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
